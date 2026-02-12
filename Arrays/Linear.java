4168941d62fa695296a4be48db9ac0244d985bc6
Bheemashankar2003 <bheemashankardiwakar324@gmail.com>
2026-02-12 10:49:09 +0530

    hello world

---
+++ Arrays/Linear.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class Linear {
+    public static void main(String[] args) {
+        
+        int [] arr = {23, 45, 65, 67, 87};
+        int ele = 23;
+
+        for(int i = 0;i<arr.length;i++){
+
+            if(arr[i] == ele)
+              System.out.println("element found at the index: "+i);
+        }
+          System.out.println("elements not found ");
+    }
+}
