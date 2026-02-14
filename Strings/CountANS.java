5cd7425489a2ac39ccb812d34e6c376f5bac75e5
Bheemashankar2003 <bheemashankardiwakar324@gmail.com>
2026-02-14 11:41:34 +0530

    programs

---
+++ Strings/CountANS.java
@@ -0,0 +1,28 @@
+package Strings;
+
+public class CountANS {
+    
+    public static void main(String[] args) {
+        
+        String s1 = "hello 345 ^()$";
+        int alpha = 0;
+        int num =0;
+        int sym = 0;
+
+        for(int i = 0;i<s1.length();i++){
+
+            char ch = s1.charAt(i);
+            if(ch>='a' && ch<='z')
+              alpha++;
+            else if(ch>='0' && ch<='9')
+              num++;
+            else
+              sym++;
+        }
+
+        System.out.println("count of alphabets: "+alpha);
+        System.out.println("count of the numbers: "+num);
+        System.out.println("count of the symbols: "+sym);
+        
+    }
+}
--- Strings/Panagram.java
+++ Strings/Panagram.java
@@ -10,20 +10,26 @@ public class Panagram {
         s = s.toLowerCase();
 
         //removing dublicates
-        for(int i = 0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch>='a' && ch<='z'){
-                alpha[ch-'a'] = true;
-            }
-        }
+        // for(int i = 0;i<s.length();i++){
+        //     char ch = s.charAt(i);
+        //     if(ch>='a' && ch<='z'){
+        //         alpha[ch-'a'] = true;
+        //     }
+        // }
 
-        //checking all the 26 charecters present
-        for(int i = 0;i<26;i++){
-          if(!alpha[i]){
-            System.out.println("is not an panagram");
-            return;
-          }
+        // //checking all the 26 charecters present
+        // for(int i = 0;i<26;i++){
+        //   if(!alpha[i]){
+        //     System.out.println("is not an panagram");
+        //     return;
+        //   }
+        // }
+        // System.out.println("is panagram");
+
+           // or //
+        for(char ch = 'a'; ch<='z';ch++){
+            if(s.indexOf(ch) == -1)
+              System.out.println("is not panagram");
         }
-        System.out.println("is panagram");
 }
 }
